dd15d188768847335e8331ac0d3f2674e4b60660
jwavangestel <jwa.van.gestel@gmail.com>
2021-03-19 02:08:19 +0100

    Werkende test

--- UserJSON/ContentView.swift
+++ UserJSON/ContentView.swift
@@ -6,10 +6,15 @@ struct ContentView: View {
     var body: some View {
         List(users) { user in
           
-                Text(user.username)
-                    .font(.headline)
-                Text(user.name)
-                    .font(.subheadline)
+            VStack {
+            Text(user.name)
+            Text(user.document.profiles[0].name)
+            Text(user.document.profiles[0].item[0].name)
+            Text(user.document.profiles[0].item[0].email)
+            Text(user.document.profiles[0].item[1].name)
+            Text(user.document.profiles[0].item[1].email)
+        }
+
         }
             .onAppear {
                 apiCall().getUsers { (users) in
--- UserJSON/userModel.swift
+++ UserJSON/userModel.swift
@@ -4,4 +4,22 @@ struct User: Codable, Identifiable {
     let id = UUID()
     let username: String
     let name: String
+    var document: Document
 }
+
+struct Document: Codable, Identifiable {
+    let id = UUID()
+    var count: Int
+    var profiles: [Profile]
+}
+
+struct Profile: Codable {
+    let id = UUID()
+    var name, email: String
+    var item: [Item]
+}
+
+struct Item: Codable {
+    var name, email: String
+}
+
