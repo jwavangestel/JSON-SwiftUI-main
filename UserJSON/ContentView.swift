f2e167d163c28f286d00ba94c2ffac706e52bc35
jwavangestel <jwa.van.gestel@gmail.com>
2021-03-19 15:29:23 +0100

    Thema scherm

--- UserJSON/ContentView.swift
+++ UserJSON/ContentView.swift
@@ -4,23 +4,27 @@ struct ContentView: View {
     @State var themas: [User] = []
 
     var body: some View {
-        List(themas) { thema in
-          
-            VStack {
-                Text(thema.name)
-                Text(thema.subthemas.subthema[0].name)
-                Text(thema.subthemas.subthema[0].items[0].name)
-                Text(thema.subthemas.subthema[0].items[0].item[0].desc)
-                Text(thema.subthemas.subthema[0].items[1].name)
-                Text(thema.subthemas.subthema[0].items[1].item[0].desc)
-        }
-
-        }
-            .onAppear {
-                apiCall().getUsers { (users) in
-                    self.themas = users
+        VStack {
+            Text("Thema's")
+                .font(.title)
+            List(themas) { thema in
+              
+                VStack {
+                    Text(thema.name)
+    //                Text(thema.subthemas.subthema[0].name)
+    //                Text(thema.subthemas.subthema[0].items[0].name)
+    //                Text(thema.subthemas.subthema[0].items[0].item[0].desc)
+    //                Text(thema.subthemas.subthema[0].items[1].name)
+    //                Text(thema.subthemas.subthema[0].items[1].item[0].desc)
                 }
+
             }
+                .onAppear {
+                        apiCall().getUsers { (users) in
+                            self.themas = users
+                          }
+            }
+        }
     }
 }
 
